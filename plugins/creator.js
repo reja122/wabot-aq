64754297bb1d1f5de66b8ab778845d515bc16f73
reja122 <85597494+reja122@users.noreply.github.com>
2021-06-09 12:14:29 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  this.sendContact(m.chat, '6285849302686', 'reja_el', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
